203a571dac74d17ed6aa82f16da5e44310097bcc
Alexadre <alexandrespmg@hotmail.com>
2020-02-02 15:43:27 -0300

    Update dtos Email and Attachment

--- src/EmailSender/Dtos/Attachment.cs
+++ src/EmailSender/Dtos/Attachment.cs
@@ -1,9 +1,11 @@
+using MimeKit;
+
 namespace EmailSender.Dtos
 {
     public class Attachment
     {
         public string Name {get;set;}
-        public string ContentType {get;set;}
+        public ContentType ContentType {get;set;}
         public byte[] File {get;set;}
     }
 }
--- src/EmailSender/Dtos/Email.cs
+++ src/EmailSender/Dtos/Email.cs
@@ -5,7 +5,7 @@
         public string Recipient {get;set;}
         public string Subject {get;set;}
         public string Message {get;set;}
-        public string NameOfSender {get;set;}
+        public string NameOfRecipient {get;set;}
         public Attachment Attachment {get;set;}
     }
 }
